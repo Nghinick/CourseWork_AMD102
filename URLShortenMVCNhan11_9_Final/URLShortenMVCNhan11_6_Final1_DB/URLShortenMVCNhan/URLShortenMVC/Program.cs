dd607aeed20f3c33ce014c6c68312354e3c43666
Trần Vĩnh Nghi <145666098+Nghinick@users.noreply.github.com>
2025-11-10 21:00:06 +0700

    Delete URLShortenMVCNhan11_9_Final/URLShortenMVCNhan11_6_Final1_DB/URLShortenMVCNhan/URLShortenMVC directory

--- URLShortenMVCNhan11_9_Final/URLShortenMVCNhan11_6_Final1_DB/URLShortenMVCNhan/URLShortenMVC/Program.cs
+++
@@ -1,4 +0,0 @@
-// Add this route mapping in the endpoint configuration (after MapControllerRoute for default)
-app.MapControllerRoute(
-    name: "areas",
-    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
--- URLShortenMVCNhan11_9_Final/URLShortenMVCNhan11_6_Final1_DB/URLShortenMVCNhan/URLShortenMVC/URLShorten/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace URLShorten.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
